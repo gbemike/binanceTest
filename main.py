f7e35f880a6f1acc1f0648cdc21b2404ef32191f
gbemike <76538077+gbemike@users.noreply.github.com>
2023-11-16 00:08:08 +0100

    next

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('binance_data.csv')
-df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = pd.to_datetime(df['Date'], format='mixed')
 df = df.set_index('Date')
-ohlc_df = df['Price'].resample('24H').ohlc()
+ohlc_df = df['Price'].resample('1H').ohlc()
 ohlc_df.to_csv('USDT_NGN rates.csv', mode='a', header=False)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,50 @@
 import pandas as pd
 import streamlit as st
-from datetime import datetime as dt
-from bokeh.plotting import figure, column
-
+from datetime import datetime as dt, timedelta as td
+from bokeh.plotting import figure, column, show
+from bokeh.models import HoverTool, NumeralTickFormatter
+import plotly.graph_objects as go
 
 st.set_page_config(layout="wide", page_title="USDT_NGN Rates Tracker")
 
 # load dataset
 @st.cache_data
 def load_dataset():
-    df = pd.read_csv("USDT_NGN rates.csv")
-    df['Date'] = df.index
-    df['BarColor'] = df[['open', 'close']].apply(lambda x: 'red' if x.open > x.close else 'green', axis=1)
-    df['DateStr'] = df.Date.astype(str) 
+    df = pd.read_csv("USDT_NGN rates.csv", parse_dates=True)
+    df['Date'] = pd.to_datetime(df['Date'])
 
     return df
 
-df = load_dataset()
+original_df = load_dataset()
 
-# create chart
-def chart(df, close_line = False):
-    # candlestick figure
-    candle =figure(x_axis_type='datetime', 
-                   x_range=(df.Date.values[0], 
-                            df.Date.values[-1]), 
-                            width='', height=500, 
-                            title='USDT_NGN', tool_tips=[('Date','@Date'), ('open','@open'), ('high','@high'), ('low','@low'), ('close','@close')])
-    
-    # creates a black line between low and high values
-    candle.segement('Date', 'low','Date', 'high', color='black', line_width=0.5, source=df)
-    # creates ....
-    candle.segement('Date', 'open','Date', 'close', line_color='BarColor', line_width=2 if len(df)>100 else 6, source=df)
+
+figure = go.Figure(
+    data = [
+        go.Candlestick(
+            x = original_df['Date'],
+            open = original_df['open'],
+            high = original_df['high'],
+            low = original_df['low'],
+            close = original_df['close'],
+            increasing_line_color = 'green',
+            decreasing_line_color = 'red'
+        )
+    ]
+)
+
+figure.update_layout(xaxis_title='Date',
+                     yaxis_title='Rates',
+                     xaxis_tickformat='',
+                     xaxis_rangeslider_visible=False)
 
 # Dashboard
 st.title("USDT_NGN Rates Tracker :tea: :coffee:")
 st.sidebar.markdown("#### Date Range Selection")
+
+st.plotly_chart(figure,theme='streamlit', use_container_width=True)
 
 col1,col2 = st.sidebar.columns(2)
 with col1:
     start_date = st.date_input(label="Start Date", value=dt(2023,1,1))
 with col2:
     end_date = st.date_input(label="End Date", value=dt(2023,12,31))
-
-
-#st.write(data)
