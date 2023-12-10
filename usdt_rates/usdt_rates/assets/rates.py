6a7295807f0d3ad4ac561e2cadbaf62cfbaf5e59
gbemike <76538077+gbemike@users.noreply.github.com>
2023-12-10 21:31:50 +0100

    definitons comment

--- usdt_rates/usdt_rates/assets/rates.py
+++ usdt_rates/usdt_rates/assets/rates.py
@@ -16,14 +16,17 @@ def raw_rates():
     """
     Raw rates gotten straight from the Binance API
     """
-
+    # USTNGN is the symbol passed to the API endpoint
     usdt_symbol = "USDTNGN"
     usdt_endpoint = f"https://api.binance.com/api/v3/ticker/price?symbol={usdt_symbol}"
-
+    
+    # gets contents of the endpoint
     usdt_prices = requests.get(usdt_endpoint).json()
 
+    # creates the data/raw directory if it doesn't exsit
     os.makedirs("data/raw", exist_ok=True)
 
+    # raw_rates.json is created in the data/raw directory, the file contains the endpoint data
     with open("data/raw/raw_rates.json", "w+") as file:
         json.dump(usdt_prices, file)
 
@@ -32,20 +35,24 @@ def usdt_rates():
     """
     Reading raw_rates json file and converting to a DataFrame
     """
-
+    # reads raw_rates.json file and stores it as a variable called raw_rates
     with open("data/raw/raw_rates.json", "r") as file:
         raw_rates = json.loads(file.read())
 
+    # symbol and price variables are intialized to their corresponding raw_rates value
+    # date is set as the time at which it's initialised
     symbol = raw_rates['symbol']
     price = raw_rates['price']
     date = datetime.now()
     
+    # creates a dataFrame containing symbol, price and date
     df = pd.DataFrame({
         'Symbol': [symbol],
         'Price': [price],
         'Date': [date]
     })
 
+    # dataFrame is converted to a csv_file called usdt_prices
     df.to_csv("data/raw/usdt_prices.csv", mode="a",index=False, header=False)
 
 
@@ -54,13 +61,21 @@ def ohlc_rates(context: AssetExecutionContext):
     """
     Produces ohlc and price change values
     """
+    # reads usdt_prices file into usdt_rates variable
     usdt_rates = pd.read_csv("data/raw/usdt_prices.csv")
 
+    # converts Date column to a datetime object
     usdt_rates['Date'] = pd.to_datetime(usdt_rates['Date'])
+
+    # converts Price column from an object toa float 
     usdt_rates["Price"] = usdt_rates["Price"].astype(float)
 
+    # set dataframe index to Date, this makes it a DateTime index
+    # we can only use the resample() function on dataframes with DateTime index
     usdt_rates = usdt_rates.set_index("Date")
 
+    # convert all price entries to its corresponding ohlc values and aggregate it by 15 minutes
     ohlc_df = usdt_rates["Price"].resample("15T").ohlc()
-    ohlc_df['changes'] = ((ohlc_df['close']) - (ohlc_df['close']).shift(1)) * 100
+
+    # convert ohlc_df dataFrame to a csv file
     ohlc_df.to_csv("data/raw/binance_ohlc_rates.csv")
--- usdt_rates/usdt_rates/jobs/__init__.py
+++ usdt_rates/usdt_rates/jobs/__init__.py
@@ -3,26 +3,35 @@ from dagster import (
     AssetSelection,
 )
 
+# AsserSelection Returns a selection that includes assets with any of the 
+# provided keys and all asset checks that target them
+
 raw_rates = AssetSelection.keys(['raw_rates'])
 usdt_rates = AssetSelection.keys(["usdt_rates"])
 ohlc_rates = AssetSelection.keys(["ohlc_rates"])
 slack_alert = AssetSelection.keys(["rate_change"])
 
+# define_asset_job() defines a dagster job
+# selection signifies what asset should is being defined it the job
+
+# raw_rates asset job
 raw_rates_job = define_asset_job(
     name="raw_rates_job",
     selection=raw_rates,
 )
 
+# usdt_rates asset job
 rates_update_job = define_asset_job(
     name="rates_update_job",
     selection=usdt_rates,
 )
-
+# ohlc_rates asset job
 ohlc_update_job = define_asset_job(
     name="ohlc_update_job",
     selection=ohlc_rates,
 )
 
+# slack_alert asset job
 slack_alert_job = define_asset_job(
     name="slack_alert_job",
     selection=slack_alert,
